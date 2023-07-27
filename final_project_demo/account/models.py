8bb973d45dc5189618d631fb341dead224e08db8
Jan Kuźma <1jankuzma@gmail.com>
2023-07-28 00:15:59 +0200

    initial database structuring

--- final_project_demo/account/models.py
+++ final_project_demo/account/models.py
@@ -1,13 +1,85 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import BaseUserManager, AbstractBaseUser
+from django.contrib.auth.models import AbstractUser, PermissionsMixin, Group, Permission
 from django.db import models
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The email field must be valid")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, password, **extra_fields)
 
 
 class Languages(models.Model):
     prefix = models.CharField(max_length=2)
     name = models.CharField(max_length=100)
 
-class TechStack(models.Model):
-    name = models.CharField(max_length=100)
+
+class EmployerUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    company_name = models.CharField(max_length=100)
+    website = models.CharField(max_length=200)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    groups = models.ManyToManyField(
+        Group,
+        related_name='employer_users',
+        related_query_name='employer_user',
+        blank=True,
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name='employer_users',
+        related_query_name='employer_user',
+        blank=True,
+    )
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['company_name', 'website']
+
+
+class JobSeekerUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    cv = models.FileField(upload_to='cv/', blank=True, null=True)
+    location = models.CharField(max_length=100)
+    language_known = models.ForeignKey(Languages, on_delete=models.CASCADE)
+    github_profile = models.URLField(max_length=200, blank=True, null=True)
+    codewars_profile = models.URLField(max_length=200, blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    groups = models.ManyToManyField(
+        Group,
+        related_name='job_seeker_users',
+        related_query_name='job_seeker_user',
+        blank=True,
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name='job_seeker_users',
+        related_query_name='job_seeker_user',
+        blank=True,
+    )
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['location']
+
 
 class JobListing(models.Model):
     job_title = models.CharField(max_length=100)
@@ -16,20 +88,9 @@ class JobListing(models.Model):
     is_active = models.BooleanField()
     employer = models.ForeignKey('EmployerUser', related_name='job_listings', on_delete=models.CASCADE)
 
-class EmployerUser(AbstractUser):
-    company_name = models.CharField(max_length=100)
-    website = models.CharField(max_length=200)
-
-class JobSeekerUser(AbstractUser):
-    cv = models.FileField()
-    location = models.CharField(max_length=100)
-    language = models.ForeignKey(Languages, related_name='job_seekers', on_delete=models.CASCADE)
-    tech_stack = models.ManyToManyField(TechStack, related_name='job_seekers', blank=True)
-    github_profile = models.URLField(max_length=200, blank=True, null=True)
-    codewars_profile = models.URLField(max_length=200, blank=True, null=True)
 
 class WorkExperience(models.Model):
-    job_seeker = models.ForeignKey(JobSeekerUser, related_name='work_experiences', on_delete=models.CASCADE)
+    experience_of_job_seeker = models.ForeignKey(JobSeekerUser, related_name='work_experiences', on_delete=models.CASCADE)
     job_title = models.CharField(max_length=100)
     company_name = models.CharField(max_length=100)
     employment_type = models.CharField(max_length=50)
@@ -38,35 +99,44 @@ class WorkExperience(models.Model):
     end_date = models.DateField(null=True, blank=True)
     job_description = models.TextField()
 
+
 class Education(models.Model):
-    job_seeker = models.ForeignKey(JobSeekerUser, related_name='educations', on_delete=models.CASCADE)
+    education_of_job_seeker = models.ForeignKey(JobSeekerUser, related_name='educations', on_delete=models.CASCADE)
     degree = models.CharField(max_length=100)
     field_of_study = models.CharField(max_length=100)
     institution = models.CharField(max_length=100)
     graduation_year = models.PositiveIntegerField()
 
+
 class Skill(models.Model):
-    job_seeker = models.ForeignKey(JobSeekerUser, related_name='skills', on_delete=models.CASCADE)
+    skill_of_job_seeker = models.ForeignKey(JobSeekerUser, related_name='skills', on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
 
+
 class Endorsement(models.Model):
-    job_seeker = models.ForeignKey(JobSeekerUser, related_name='endorsements_received', on_delete=models.CASCADE)
+    endorsement_of_job_seeker = models.ForeignKey(JobSeekerUser, related_name='endorsements_received', on_delete=models.CASCADE)
     endorsed_by = models.ForeignKey(JobSeekerUser, related_name='endorsements_given', on_delete=models.CASCADE)
     skill = models.ForeignKey(Skill, on_delete=models.CASCADE)
 
+
 class Recommendation(models.Model):
-    job_seeker = models.ForeignKey(JobSeekerUser, related_name='received_recommendations', on_delete=models.CASCADE)
+    recommendation_of_job_seeker = models.ForeignKey(JobSeekerUser, related_name='received_recommendations', on_delete=models.CASCADE)
     given_by = models.ForeignKey(EmployerUser, related_name='given_recommendations', on_delete=models.CASCADE)
     content = models.TextField()
 
+
 class Certification(models.Model):
-    job_seeker = models.ForeignKey(JobSeekerUser, related_name='certifications', on_delete=models.CASCADE)
+    certification_of_job_seeker = models.ForeignKey(JobSeekerUser, related_name='certifications', on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     issuing_organization = models.CharField(max_length=100)
     issue_date = models.DateField()
     expiration_date = models.DateField(null=True, blank=True)
 
+
 class JobApplication(models.Model):
-    job_seeker = models.ForeignKey(JobSeekerUser, related_name='job_applications', on_delete=models.CASCADE)
+    job_seekers = models.ManyToManyField(JobSeekerUser, related_name='job_applications')
     job_listing = models.ForeignKey(JobListing, related_name='applications', on_delete=models.CASCADE)
     application_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ['job_listing', 'application_date']
